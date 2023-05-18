95b43837b2d795a6326ea859f41f073c11dc599c
Maxim <maxim.kliakhin@gmail.com>
2023-05-18 17:21:52 +0300

    +

--- level3-lesson1.php
+++ level3-lesson1.php
@@ -17,17 +17,30 @@ function task2(){
     return $nums;
 }
     
-function task3(){
+function task3() {
     $arr = [
         [2, 1, 4, 3, 5],
         [3, 5, 2, 4, 1],
         [4, 3, 1, 5, 2],
     ];
-    foreach($arr as $elem){
+
+    foreach ($arr as &$elem) {
+        $sorted = false;
+        while (!$sorted) {
+            $sorted = true;
+            for ($i = 0; $i < count($elem) - 1; $i++) {
+                if ($elem[$i] > $elem[$i + 1]) {
+                    $temp = $elem[$i];
+                    $elem[$i] = $elem[$i + 1];
+                    $elem[$i + 1] = $temp;
+                    $sorted = false;
+                }
+            }
+        }
     }
 
-        
+    return $arr;
 }
     
 
-print_r(task2());
+print_r(task3());
